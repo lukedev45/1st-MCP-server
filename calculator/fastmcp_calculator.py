19adff4f7fd4eba121323539cd9eeddfa877e0d5
luke <lukedevlin45@gmail.com>
2025-10-18 21:49:53 +0100

    Created an MCP Server for RSS feed for TheQuantumInsider, Qiskit YouTube Videos and an MCP server for a generic calculator

--- calculator/fastmcp_calculator.py
+++ calculator/fastmcp_calculator.py
@@ -56,10 +56,10 @@ def divide(a: float, b: float) -> float:
 
 # To run with STDIO interface (default)
 
-# if __name__ == "__main__":
-#     mcp.run() #STDIO by default
-
-# To run with HTTP interface
-
 if __name__ == "__main__":
-    mcp.run(transport="http", host="localhost", port=8002)
+    mcp.run() #STDIO by default
+
+# # To run with HTTP interface
+
+# if __name__ == "__main__":
+#     mcp.run(transport="http", host="localhost", port=8002)
